1e9afac393f46fb7899b0a8ac47da31f84e9b6e0
PacoAcun <103969393+PacoAcun@users.noreply.github.com>
2025-05-24 11:37:44 -0600

    Proyecto 3

--- autodf/src/autodf/pipelines/data_science/pipeline.py
+++ autodf/src/autodf/pipelines/data_science/pipeline.py
@@ -11,7 +11,7 @@ from .nodes import split_dataset, train_model, evaluate_model, train_model_with_
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(func=split_dataset, inputs=['autodf_scaled', 'params:test_size'], outputs=['X_train', 'X_test', 'y_train', 'y_test'], name='split_datasetnode'),
-        node(func=train_model, inputs=['X_train', 'y_train'], outputs='model', name='train_model_node'),
+        node(func=train_model,inputs=['X_train','y_train'],outputs='model',name='train_model_node'),
         node(func=evaluate_model, inputs=['model','X_test', 'y_test'], outputs=None, name='evaluate_model_node'),    
         node(func=train_model_with_gridsearch, inputs=['X_train', 'y_train'], outputs=['optimized_model', 'best_params'], name='train_optimized_model_node'),
         node(func=train_random_forest, inputs=['X_train', 'y_train'], outputs='rf_model', name='train_random_forest_node'),
